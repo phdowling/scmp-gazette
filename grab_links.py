7bbc7a3db117a6e26258d3c0c1239f7f1aa28dcd
Philipp Dowling <philipp.dowling@quartett-mobile.de>
2014-11-05 19:27:07 +0800

    just write list instead of downloading

--- grab_links.py
+++ grab_links.py
@@ -3,25 +3,18 @@ __author__ = 'dowling'
 import requests
 from bs4 import BeautifulSoup, SoupStrainer
 
+print "downloading index.."
 response = requests.get("http://data.jmsc.hku.hk/hongkong/gazette/pdfs/")
 soup = BeautifulSoup(response.text, parse_only=SoupStrainer("a"))
 
 pdfs = [a.attrs["href"] for a in soup.findAll("a") if a.attrs["href"].endswith(".pdf")]
 links = ["http://www.gld.gov.hk/egazette/pdf/%s/%s" % (name.split("_")[0], name.split("_")[1]) for name in pdfs]
+print "starting to grab pdfs"
 
-responses = []
-count = 0
-for pdf, link in zip(pdfs, links):
-    if count % 100 == 0:
-        print "Fetched %s documents.." % count
-    count += 1
-    responses.append(requests.get(link, stream=True))
+with open("links.txt", "w") as out:
+    for link in links:
+        out.write(link + "\n")
 
-for response, pdf in zip(responses, pdfs):
-    with open("pdfs/%s" % pdf, 'w') as handle:
-        if not response.ok:
-            print "Couldn't grab %s (%s)" % (pdf, link)
-        for block in response.iter_content(1024):
-            if not block:
-                break
-            handle.write(block)
+with open("fnames.txt", "w") as out:
+    for pdf in pdfs:
+        out.write(pdf + "\n")
