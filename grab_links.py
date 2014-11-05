be8c6f3a67d56069562bbf00896a15e83243bf48
Philipp Dowling <philipp.dowling@quartett-mobile.de>
2014-11-05 21:22:36 +0800

    just write list instead of downloading

--- grab_links.py
+++ grab_links.py
@@ -8,7 +8,8 @@ response = requests.get("http://data.jmsc.hku.hk/hongkong/gazette/pdfs/")
 soup = BeautifulSoup(response.text, parse_only=SoupStrainer("a"))
 
 pdfs = [a.attrs["href"] for a in soup.findAll("a") if a.attrs["href"].endswith(".pdf")]
-links = ["http://www.gld.gov.hk/egazette/pdf/%s/%s" % (name.split("_")[0], name.split("_")[1]) for name in pdfs]
+#links = ["http://www.gld.gov.hk/egazette/pdf/%s/%s" % (name.split("_")[0], name.split("_")[1]) for name in pdfs]
+links = ["http://data.jmsc.hku.hk/hongkong/gazette/pdfs/%s" % name for name in pdfs]
 print "starting to grab pdfs"
 
 with open("links.txt", "w") as out:
