373457b76722e290f2254e11810fb7cc1147b710
Philipp Dowling <philipp.dowling@quartett-mobile.de>
2014-11-05 19:01:08 +0800

    working on downloader

--- grab_links.py
+++ grab_links.py
@@ -9,9 +9,16 @@ soup = BeautifulSoup(response.text, parse_only=SoupStrainer("a"))
 pdfs = [a.attrs["href"] for a in soup.findAll("a") if a.attrs["href"].endswith(".pdf")]
 links = ["http://www.gld.gov.hk/egazette/pdf/%s/%s" % (name.split("_")[0], name.split("_")[1]) for name in pdfs]
 
+responses = []
+count = 0
 for pdf, link in zip(pdfs, links):
+    if count % 1000 == 0:
+        print "Fetched %s document.." % count
+    count += 1
+    responses.append(requests.get(link, stream=True))
+
+for response, pdf in zip(responses, pdfs):
     with open("pdfs/%s" % pdf, 'w') as handle:
-        response = requests.get(link, stream=True)
         if not response.ok:
             print "Couldn't grab %s (%s)" % (pdf, link)
         for block in response.iter_content(1024):
